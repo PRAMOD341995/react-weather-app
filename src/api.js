098fa5771eed426fbd73c399875ec46471c3bed1
PRAMOD341995 <140843651+PRAMOD341995@users.noreply.github.com>
2023-12-08 08:00:09 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,15 @@
+
+
+export const geoApiOptions = {
+	method: "GET",
+	headers: {
+		'X-RapidAPI-Key': "172e4d17cbmshd3c8322201d7435p119790jsn1034cffb3789",
+		'X-RapidAPI-Host': "wft-geo-db.p.rapidapi.com" ,
+	},
+};
+
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL ="https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY ="f05bde800e213f6f129022cb58a60d65";
